9ec06c2dc17fbfaf5428d4c6e87e0820debdaac4
tamilarasanm676-crypto <tamilarasanm676@gmail.com>
2025-12-12 16:16:36 +0530

    Implement ETL flow with extract, transform, load tasks

---
+++ batch_job.py
@@ -0,0 +1,33 @@
+from prefect import flow, task
+
+
+@task
+
+def extract():
+
+return "Data Extracted"
+
+
+@task
+
+def transform(data):
+
+return f"{data} → Transformed"
+
+
+@task
+
+def load(data):
+
+return f"{data}" # logs will NOT appear in Prefect Cloud
+
+
+@flow
+
+def etl_flow(job_name: str = "Default Job"):
+
+raw = extract()
+
+processed = transform(raw)
+
+load(f"{job_name}: {processed}")
