4ee9b77f047091b6a5998399d495768243ae1931
tamilarasanm676-crypto <tamilarasanm676@gmail.com>
2025-12-12 16:19:37 +0530

    Refactor extract, transform, and load functions
    Refactor ETL functions to return values directly.

--- batch_job.py
+++ batch_job.py
@@ -4,29 +4,25 @@ from prefect import flow, task
 @task
 
 def extract():
-
-return "Data Extracted"
+  return "Data Extracted"
 
 
 @task
 
 def transform(data):
-
-return f"{data} → Transformed"
+  return f"{data} → Transformed"
 
 
 @task
 
 def load(data):
-
-return f"{data}" # logs will NOT appear in Prefect Cloud
+  return f"{data}" # logs will NOT appear in Prefect Cloud
 
 
 @flow
 
 def etl_flow(job_name: str = "Default Job"):
-
-raw = extract()
+  raw = extract()
 
 processed = transform(raw)
 
