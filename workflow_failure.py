9cb2a056a974eeec09fbffe3ce3487b9f32fbc2c
tamilarasanm676-crypto <tamilarasanm676@gmail.com>
2025-12-17 00:00:19 +0530

    Update workflow_failure.py

--- workflow_failure.py
+++ workflow_failure.py
@@ -3,6 +3,10 @@ from prefect import flow, task
 @task
 def failing_task():
     raise Exception("Simulated failure")
+@task(retries=3, retry_delay_seconds=5)
+def retry_task():
+    raise Exception("Retrying")
+    
 
 @flow
 def failure_workflow():
